c27a06028505319c903b84030175800ca06b80f7
Karolina <trallala00@gmail.com>
2021-07-13 22:58:36 +0200

    Add image resize function

--- excersise_three.py
+++ excersise_three.py
@@ -1,8 +1,13 @@
+#resizing image
 import cv2
 import numpy as np
 
 img = cv2.imread("resources/orange.jpg")
+print(img.shape)
+
+imgResize = cv2.resize(img,(300,200))
 
 cv2.imshow("Image", img)
+cv2.imshow("Image Resize", imgResize)
 
 cv2.waitKey(0)
