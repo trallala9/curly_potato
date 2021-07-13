d80ae366f2beac5bc499c0b7d4792df787b45fab
Karolina <trallala00@gmail.com>
2021-07-13 21:58:48 +0200

    Add dialaton

--- excersise_two.py
+++ excersise_two.py
@@ -7,10 +7,11 @@ kernel = np.ones((5,5),np.uint8)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7), 0)
 imgCanny = cv2.Canny(img,100,100)
-imgDialation = cv2.dilate(imgCanny,)
+imgDialation = cv2.dilate(imgCanny,kernel, iterations=0.5)
 
 
 cv2.imshow('Gray image', imgGray)
 cv2.imshow('Blur image', imgBlur)
 cv2.imshow('Canny Image', imgCanny)
+cv2.imshow('Dialation Image', imgDialation)
 cv2.waitKey(0)
