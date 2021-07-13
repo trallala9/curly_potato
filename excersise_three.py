edeef709894140d0bbbeeed0ad12fd5ff5c08421
Karolina <trallala00@gmail.com>
2021-07-13 23:09:45 +0200

    Add image cropp

--- excersise_three.py
+++ excersise_three.py
@@ -6,8 +6,9 @@ img = cv2.imread("resources/orange.jpg")
 print(img.shape)
 
 imgResize = cv2.resize(img,(300,200))
+imgCropped = img[0:200, 200:500]
 
 cv2.imshow("Image", img)
-cv2.imshow("Image Resize", imgResize)
-
+cv2.imshow("Image Resized", imgResize)
+cv2.imshow("Image Cropped", imgCropped)
 cv2.waitKey(0)
