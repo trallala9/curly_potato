ad6a4a48c0edbb9138b13c009bb4a3b22c6bd0db
Karolina <trallala00@gmail.com>
2021-07-20 18:33:38 +0200

    Add cv2.rectangle and circle func to draw elements

--- excersize_four.py
+++ excersize_four.py
@@ -7,6 +7,8 @@ img = np.zeros((512, 512, 3), np.uint8)
 
 cv2.line(img,(0, 0),(300,300),(0, 255, 255),3)
 cv2.line(img,(0, 0),(img.shape[1], img.shape[0]),(0, 255, 255),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0, 255),2)
+cv2.circle(img,(400,50),30,(255,255,0),5)
 
 cv2.imshow("Image", img)
 
