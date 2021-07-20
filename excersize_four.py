2ccf1828762c316d282730d5f9d5208bc131382a
Karolina <trallala00@gmail.com>
2021-07-20 17:55:46 +0200

    Add couloured image, range, channels

--- excersize_four.py
+++ excersize_four.py
@@ -2,8 +2,9 @@
 import cv2
 import numpy as np
 
-img = np.zeros((512, 512))
-
+img = np.zeros((512, 512, 3), np.uint8)
+img[200:300, 100:200] = 255,0,0
 cv2.imshow("Image", img)
+
 
 cv2.waitKey(0)
