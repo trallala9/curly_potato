678cb2f96ab3644c280ae99be8ecf05b74bb3549
Karolina <trallala00@gmail.com>
2021-07-20 18:47:28 +0200

    add cv2.putText func. to write  text on image

--- excersize_four.py
+++ excersize_four.py
@@ -9,6 +9,7 @@ cv2.line(img,(0, 0),(300,300),(0, 255, 255),3)
 cv2.line(img,(0, 0),(img.shape[1], img.shape[0]),(0, 255, 255),3)
 cv2.rectangle(img,(0,0),(250,350),(0,0, 255),2)
 cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img, "Moj tekst",(200,100), cv2.FONT_HERSHEY_TRIPLEX,1,(0,0,255),1)
 
 cv2.imshow("Image", img)
 
