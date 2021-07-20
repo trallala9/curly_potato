f2917083bb78a2778c3061f951444033cebb4c27
Karolina <trallala00@gmail.com>
2021-07-20 18:21:20 +0200

    draw line aacross the image using cv2.shape func

--- excersize_four.py
+++ excersize_four.py
@@ -6,6 +6,7 @@ img = np.zeros((512, 512, 3), np.uint8)
 #img[200:300, 100:200] = 255,0,0
 
 cv2.line(img,(0, 0),(300,300),(0, 255, 255),3)
+cv2.line(img,(0, 0),(img.shape[1], img.shape[0]),(0, 255, 255),3)
 
 cv2.imshow("Image", img)
 
